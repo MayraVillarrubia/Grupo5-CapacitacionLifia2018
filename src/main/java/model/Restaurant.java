aea92e0dd95e1385bc846aafd61a4ae256847f85
Mayra Villarrubia <mayravillarrubia@gmail.com>
2018-06-02 00:47:11 -0300

    clases sin métodos aún

---
+++ src/main/java/model/Restaurant.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.List;
+
+public class Restaurant {
+	private String nombre;
+	private Category category;
+	private List<Comment> comments;
+	private Responsible responsible;
+	//private  Ubicacion -> de qué forma la representamos??
+	
+
+}
